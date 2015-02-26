760dcab58d7bcb41b3d81467aebf99dfd846c164
zhenghonglin <289797549@gmail.com>
2015-02-26 18:24:23 +0800

    工程创建

---
+++ src/com/example/folderviewpagerdemo/WeiBoActivity.java
@@ -0,0 +1,182 @@
+package com.example.folderviewpagerdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class WeiBoActivity extends Activity {
+
+	private ViewPager viewPager;//ҳ������
+	private ImageView imageView;// ����ͼƬ
+	private TextView textView1,textView2,textView3;
+	private List<View> views;// Tabҳ���б�
+	private int offset = 0;// ����ͼƬƫ����
+	private int currIndex = 0;// ��ǰҳ�����
+	private int bmpW;// ����ͼƬ���
+	private View view1,view2,view3;//����ҳ��
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.weibo);
+		InitImageView();
+		InitTextView();
+		InitViewPager();
+	}
+
+	private void InitViewPager() {
+		viewPager=(ViewPager) findViewById(R.id.vPager);
+		views=new ArrayList<View>();
+		LayoutInflater inflater=getLayoutInflater();
+		view1=inflater.inflate(R.layout.lay1, null);
+		view2=inflater.inflate(R.layout.lay2, null);
+		view3=inflater.inflate(R.layout.lay3, null);
+		views.add(view1);
+		views.add(view2);
+		views.add(view3);
+		viewPager.setAdapter(new MyViewPagerAdapter(views));
+		viewPager.setCurrentItem(0);
+		viewPager.setOnPageChangeListener(new MyOnPageChangeListener());
+	}
+	 /**
+	  *  ��ʼ��ͷ��
+	  */
+
+	private void InitTextView() {
+		textView1 = (TextView) findViewById(R.id.text1);
+		textView2 = (TextView) findViewById(R.id.text2);
+		textView3 = (TextView) findViewById(R.id.text3);
+
+		textView1.setOnClickListener(new MyOnClickListener(0));
+		textView2.setOnClickListener(new MyOnClickListener(1));
+		textView3.setOnClickListener(new MyOnClickListener(2));
+	}
+
+	/**
+	 2      * ��ʼ������
+	 3 */
+
+	private void InitImageView() {
+		imageView= (ImageView) findViewById(R.id.cursor);
+		bmpW = BitmapFactory.decodeResource(getResources(), R.drawable.a).getWidth();// ��ȡͼƬ���
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		int screenW = dm.widthPixels;// ��ȡ�ֱ��ʿ��
+		offset = (screenW / 3 - bmpW) / 2;// ����ƫ����
+		Matrix matrix = new Matrix();
+		matrix.postTranslate(offset, 0);
+		imageView.setImageMatrix(matrix);// ���ö�����ʼλ��
+	}
+
+	/** 
+	 *     
+	 * ͷ�������� 3 */
+	private class MyOnClickListener implements OnClickListener{
+        private int index=0;
+        public MyOnClickListener(int i){
+        	index=i;
+        }
+		public void onClick(View v) {
+			viewPager.setCurrentItem(index);			
+		}
+		
+	}
+	
+	public class MyViewPagerAdapter extends PagerAdapter{
+		private List<View> mListViews;
+		
+		public MyViewPagerAdapter(List<View> mListViews) {
+			this.mListViews = mListViews;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) 	{	
+			container.removeView(mListViews.get(position));
+		}
+
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {			
+			 container.addView(mListViews.get(position), 0);
+			 return mListViews.get(position);
+		}
+
+		@Override
+		public int getCount() {			
+			return  mListViews.size();
+		}
+		
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {			
+			return arg0==arg1;
+		}
+	}
+
+    public class MyOnPageChangeListener implements OnPageChangeListener{
+
+    	int one = offset * 2 + bmpW;// ҳ��1 -> ҳ��2 ƫ����
+		int two = one * 2;// ҳ��1 -> ҳ��3 ƫ����
+		public void onPageScrollStateChanged(int arg0) {
+			
+			
+		}
+
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+			
+			
+		}
+
+		public void onPageSelected(int arg0) {
+			/*
+			Animation animation = null;
+			switch (arg0) {
+			case 0:
+				if (currIndex == 1) {
+					animation = new TranslateAnimation(one, 0, 0, 0);
+				} else if (currIndex == 2) {
+					animation = new TranslateAnimation(two, 0, 0, 0);
+				}
+				break;
+			case 1:
+				if (currIndex == 0) {
+					animation = new TranslateAnimation(offset, one, 0, 0);
+				} else if (currIndex == 2) {
+					animation = new TranslateAnimation(two, one, 0, 0);
+				}
+				break;
+			case 2:
+				if (currIndex == 0) {
+					animation = new TranslateAnimation(offset, two, 0, 0);
+				} else if (currIndex == 1) {
+					animation = new TranslateAnimation(one, two, 0, 0);
+				}
+				break;
+				
+			}
+			*/
+			Animation animation = new TranslateAnimation(one*currIndex, one*arg0, 0, 0);
+			currIndex = arg0;
+			animation.setFillAfter(true);// True:ͼƬͣ�ڶ�������λ��
+			animation.setDuration(300);
+			imageView.startAnimation(animation);
+			Toast.makeText(WeiBoActivity.this, "��ѡ����"+ viewPager.getCurrentItem()+"ҳ��", Toast.LENGTH_SHORT).show();
+		}
+    	
+    }
+}
