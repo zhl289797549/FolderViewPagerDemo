98189db563b088753c6a7a7c674b7c8341206347
zhenghonglin <289797549@gmail.com>
2015-02-28 18:21:34 +0800

    注释测试

--- src/com/example/folderviewpagerdemo/view/grid/DynamicGridView.java
+++ src/com/example/folderviewpagerdemo/view/grid/DynamicGridView.java
@@ -16,6 +16,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.View;
@@ -34,11 +35,6 @@ import com.nineoldandroids.animation.ObjectAnimator;
 import com.nineoldandroids.animation.TypeEvaluator;
 import com.nineoldandroids.animation.ValueAnimator;
 
-/**
- * Author: alex askerov
- * Date: 9/6/13
- * Time: 12:31 PM
- */
 public class DynamicGridView extends GridView {
     private static final int INVALID_ID = -1;
 
@@ -46,24 +42,57 @@ public class DynamicGridView extends GridView {
     private static final int SMOOTH_SCROLL_AMOUNT_AT_EDGE = 8;
 
     private BitmapDrawable mHoverCell;
+    /**
+     * HoverCell 就是拖动起来的View   的当前边界坐标
+     */
     private Rect mHoverCellCurrentBounds;
+    /**
+     * HoverCell 就是拖动起来的View   的最初边界坐标
+     */
     private Rect mHoverCellOriginalBounds;
 
+    /**
+     * 从移动开始偏移的y总值
+     */
     private int mTotalOffsetY = 0;
+    /**
+     * 从移动开始偏移的x总值
+     */
     private int mTotalOffsetX = 0;
 
+    /**
+     * 手指点击下的坐标 x坐标 相对于父类view
+     */
     private int mDownX = -1;
+    /**
+     * 手指点击下的坐标 y坐标 相对于父类view
+     */
     private int mDownY = -1;
+    /**
+     * 移动后的y 相对于父类view
+     */
     private int mLastEventY = -1;
+    /**
+     * 移动后的x 相对于父类view
+     */
     private int mLastEventX = -1;
 
     //used to distinguish straight line and diagonal switching
     private int mOverlapIfSwitchStraightLine;
 
+    /**
+     * 记录可见view的itemId集合 除了拖动起来的view
+     */
     private List<Long> idList = new ArrayList<Long>();
 
+    /**
+     * 记录Adapter中的View的getItemId 移动的view的id
+     */
     private long mMobileItemId = INVALID_ID;
 
+    /**
+     * cell是否在移动  长按的view不可见的时候 设置view正在移动为true
+     */
     private boolean mCellIsMobile = false;
     private int mActivePointerId = INVALID_ID;
 
@@ -73,15 +102,16 @@ public class DynamicGridView extends GridView {
     private int mScrollState = OnScrollListener.SCROLL_STATE_IDLE;
 
     private boolean mIsEditMode = false;
-    private List<ObjectAnimator> mWobbleAnimators = new LinkedList<ObjectAnimator>();
     private boolean mHoverAnimation;
     private boolean mReorderAnimation;
-    private boolean mWobbleInEditMode = true;
     private boolean mIsEditModeEnabled = true;
 
     private OnScrollListener mUserScrollListener;
     private OnDropListener mDropListener;
     private OnDragListener mDragListener;
+    /**
+     * 编辑模式监听  长按后变成edit模式
+     */
     private OnEditModeChangeListener mEditModeChangeListener;
 
     private OnItemClickListener mUserItemClickListener;
@@ -97,6 +127,9 @@ public class DynamicGridView extends GridView {
     private Stack<DynamicGridModification> mModificationStack;
     private DynamicGridModification mCurrentModification;
 
+    /**
+     * 在view的btimapdrawable的创建前后的监听
+     */
     private OnSelectedItemBitmapCreationListener mSelectedItemBitmapCreationListener;
     private View mMobileView;
 
@@ -140,13 +173,17 @@ public class DynamicGridView extends GridView {
      * Start edit mode with position. Useful for start edit mode in
      * {@link android.widget.AdapterView.OnItemClickListener}
      * or {@link android.widget.AdapterView.OnItemLongClickListener}
+     * view 长按入口
      */
     public void startEditMode(int position) {
+    	Log.e("zhenghonglin","startEditMode==="+position);
         if (!mIsEditModeEnabled)
             return;
+        Log.e("zhenghonglin","startEditMode===");
+        /**
+         * AbsListView方法 阻拦父层的View截获touch事务 之后的事件直接传递给DynamicGrid的onTouchEvent
+         */
         requestDisallowInterceptTouchEvent(true);
-        if (isPostHoneycomb() && mWobbleInEditMode)
-            startWobbleAnimation();
         if (position != -1) {
             startDragAtPosition(position);
         }
@@ -158,8 +195,6 @@ public class DynamicGridView extends GridView {
     public void stopEditMode() {
         mIsEditMode = false;
         requestDisallowInterceptTouchEvent(false);
-        if (isPostHoneycomb() && mWobbleInEditMode)
-            stopWobble(true);
         if (mEditModeChangeListener != null)
             mEditModeChangeListener.onEditModeChanged(false);
     }
@@ -178,14 +213,6 @@ public class DynamicGridView extends GridView {
 
     public boolean isEditMode() {
         return mIsEditMode;
-    }
-
-    public boolean isWobbleInEditMode() {
-        return mWobbleInEditMode;
-    }
-
-    public void setWobbleInEditMode(boolean wobbleInEditMode) {
-        this.mWobbleInEditMode = wobbleInEditMode;
     }
 
     @Override
@@ -253,86 +280,12 @@ public class DynamicGridView extends GridView {
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void startWobbleAnimation() {
-        for (int i = 0; i < getChildCount(); i++) {
-            View v = getChildAt(i);
-            if (v != null && Boolean.TRUE != v.getTag(R.id.dgv_wobble_tag)) {
-                if (i % 2 == 0)
-                    animateWobble(v);
-                else
-                    animateWobbleInverse(v);
-                v.setTag(R.id.dgv_wobble_tag, true);
-            }
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void stopWobble(boolean resetRotation) {
-        for (Animator wobbleAnimator : mWobbleAnimators) {
-            wobbleAnimator.cancel();
-        }
-        mWobbleAnimators.clear();
-        for (int i = 0; i < getChildCount(); i++) {
-            View v = getChildAt(i);
-            if (v != null) {
-                if (resetRotation) v.setRotation(0);
-                v.setTag(R.id.dgv_wobble_tag, false);
-            }
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void restartWobble() {
-        stopWobble(false);
-        startWobbleAnimation();
-    }
-
     public void init(Context context) {
         super.setOnScrollListener(mScrollListener);
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
         mSmoothScrollAmountAtEdge = (int) (SMOOTH_SCROLL_AMOUNT_AT_EDGE * metrics.density + 0.5f);
         mOverlapIfSwitchStraightLine = getResources().getDimensionPixelSize(R.dimen.dgv_overlap_if_switch_straight_line);
     }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void animateWobble(View v) {
-        ObjectAnimator animator = createBaseWobble(v);
-        animator.setFloatValues(-2, 2);
-        animator.start();
-        mWobbleAnimators.add(animator);
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void animateWobbleInverse(View v) {
-        ObjectAnimator animator = createBaseWobble(v);
-        animator.setFloatValues(2, -2);
-        animator.start();
-        mWobbleAnimators.add(animator);
-    }
-
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private ObjectAnimator createBaseWobble(final View v) {
-
-        if (!isPreLollipop())
-            v.setLayerType(LAYER_TYPE_SOFTWARE, null);
-
-        ObjectAnimator animator = new ObjectAnimator();
-        animator.setDuration(180);
-        animator.setRepeatMode(ValueAnimator.REVERSE);
-        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.setPropertyName("rotation");
-        animator.setTarget(v);
-        animator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                v.setLayerType(LAYER_TYPE_NONE, null);
-            }
-        });
-        return animator;
-    }
-
 
     private void reorderElements(int originalPosition, int targetPosition) {
         if (mDragListener != null)
@@ -352,6 +305,7 @@ public class DynamicGridView extends GridView {
      * Creates the hover cell with the appropriate bitmap and of appropriate
      * size. The hover cell's BitmapDrawable is drawn on top of the bitmap every
      * single time an invalidate call is made.
+     * 根据v生成BitmapDrawable
      */
     private BitmapDrawable getAndAddHoverView(View v) {
 
@@ -382,7 +336,10 @@ public class DynamicGridView extends GridView {
         return bitmap;
     }
 
-
+    /**
+     * 根据itemId 更新相邻view  就是把可见view 的itemId加到idList中去
+     * @param itemId
+     */
     private void updateNeighborViewsForId(long itemId) {
         idList.clear();
         int draggedPos = getPositionForID(itemId);
@@ -395,16 +352,23 @@ public class DynamicGridView extends GridView {
 
     /**
      * Retrieves the position in the grid corresponding to <code>itemId</code>
+     * 根据itemId取positon
      */
     public int getPositionForID(long itemId) {
         View v = getViewForId(itemId);
         if (v == null) {
             return -1;
         } else {
+        	//AdapterView方法 根据V获得v在adapter中的position
             return getPositionForView(v);
         }
     }
 
+    /**
+     * 根据itemId对应View
+     * @param itemId
+     * @return
+     */
     public View getViewForId(long itemId) {
         int firstVisiblePosition = getFirstVisiblePosition();
         ListAdapter adapter = getAdapter();
@@ -423,14 +387,19 @@ public class DynamicGridView extends GridView {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
+            	Log.e("zhenghonglin","onTouchEvent===");
                 mDownX = (int) event.getX();
                 mDownY = (int) event.getY();
+                //第一个手指
                 mActivePointerId = event.getPointerId(0);
                 if (mIsEditMode && isEnabled()) {
+                	//GridView 方法 布局子view
                     layoutChildren();
+                    //AbsListView 方法寻找 x y 对应view 的position
                     int position = pointToPosition(mDownX, mDownY);
                     startDragAtPosition(position);
                 } else if (!isEnabled()) {
+                	//不在编辑模式下 且DynamicGridView不可被操作 返回false 不再接收touch事件
                     return false;
                 }
 
@@ -452,7 +421,7 @@ public class DynamicGridView extends GridView {
                     mHoverCellCurrentBounds.offsetTo(mHoverCellOriginalBounds.left + deltaX + mTotalOffsetX,
                             mHoverCellOriginalBounds.top + deltaY + mTotalOffsetY);
                     mHoverCell.setBounds(mHoverCellCurrentBounds);
-                    invalidate();
+                    invalidate();//会回掉到dispatchDraw 绘制拖动起来的view
                     handleCellSwitch();
                     mIsMobileScrolling = false;
                     handleMobileCellScroll();
@@ -507,9 +476,14 @@ public class DynamicGridView extends GridView {
         return super.onTouchEvent(event);
     }
 
+    /**
+     * 开始拖动指定position的View
+     * @param position
+     */
     private void startDragAtPosition(int position) {
         mTotalOffsetY = 0;
         mTotalOffsetX = 0;
+        //得到position对应的selectedView
         int itemNum = position - getFirstVisiblePosition();
         View selectedView = getChildAt(itemNum);
         if (selectedView != null) {
@@ -519,8 +493,10 @@ public class DynamicGridView extends GridView {
             mHoverCell = getAndAddHoverView(selectedView);
             if (mSelectedItemBitmapCreationListener != null)
                 mSelectedItemBitmapCreationListener.onPostSelectedItemBitmapCreation(selectedView, position, mMobileItemId);
+            //将拖动的view 设置为隐藏占用位置  这里isPostHoneycomb（） 有待测试
             if (isPostHoneycomb())
                 selectedView.setVisibility(View.INVISIBLE);
+            //长按的view不可见的时候 设置view正在移动为true
             mCellIsMobile = true;
             updateNeighborViewsForId(mMobileItemId);
             if (mDragListener != null) {
@@ -634,13 +610,6 @@ public class DynamicGridView extends GridView {
         mMobileItemId = INVALID_ID;
         mobileView.setVisibility(View.VISIBLE);
         mHoverCell = null;
-        if (isPostHoneycomb() && mWobbleInEditMode) {
-            if (mIsEditMode) {
-                restartWobble();
-            } else{
-                stopWobble(true);
-            }
-        }
         //ugly fix for unclear disappearing items after reorder
         for (int i = 0; i < getLastVisiblePosition() - getFirstVisiblePosition(); i++) {
             View child = getChildAt(i);
@@ -686,6 +655,9 @@ public class DynamicGridView extends GridView {
 
     }
 
+    /**
+     * 交换动画
+     */
     private void handleCellSwitch() {
         final int deltaY = mLastEventY - mDownY;
         final int deltaX = mLastEventX - mDownX;
@@ -753,6 +725,7 @@ public class DynamicGridView extends GridView {
             else                                //Android L
                 switchCellAnimator = new LSwitchCellAnimator(deltaX, deltaY);
 
+            switchCellAnimator = new LSwitchCellAnimator(deltaX, deltaY);
             updateNeighborViewsForId(mMobileItemId);
 
             switchCellAnimator.animateSwitchCell(originalPosition, targetPosition);
@@ -903,6 +876,11 @@ public class DynamicGridView extends GridView {
         return targetColumnRowPair.y == mobileColumnRowPair.y && targetColumnRowPair.x < mobileColumnRowPair.x;
     }
 
+    /**
+     * 获取View对应的pos的 行列值
+     * @param view
+     * @return
+     */
     private Point getColumnAndRowForView(View view) {
         int pos = getPositionForView(view);
         int columns = getColumnCount();
@@ -984,10 +962,24 @@ public class DynamicGridView extends GridView {
         void onActionDrop();
     }
 
+    /**
+     * 拖动监听
+     * @author zhenghonglin_91
+     *
+     */
     public interface OnDragListener {
 
+    	/**
+    	 * 开始拖动
+    	 * @param position
+    	 */
         public void onDragStarted(int position);
 
+        /**
+         * 位置改变
+         * @param oldPosition
+         * @param newPosition
+         */
         public void onDragPositionsChanged(int oldPosition, int newPosition);
     }
 
@@ -1032,32 +1024,8 @@ public class DynamicGridView extends GridView {
 
             mPreviousFirstVisibleItem = mCurrentFirstVisibleItem;
             mPreviousVisibleItemCount = mCurrentVisibleItemCount;
-            if (isPostHoneycomb() && mWobbleInEditMode) {
-                updateWobbleState(visibleItemCount);
-            }
             if (mUserScrollListener != null) {
                 mUserScrollListener.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
-            }
-        }
-
-        @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-        private void updateWobbleState(int visibleItemCount) {
-            for (int i = 0; i < visibleItemCount; i++) {
-                View child = getChildAt(i);
-
-                if (child != null) {
-                    if (mMobileItemId != INVALID_ID && Boolean.TRUE != child.getTag(R.id.dgv_wobble_tag)) {
-                        if (i % 2 == 0)
-                            animateWobble(child);
-                        else
-                            animateWobbleInverse(child);
-                        child.setTag(R.id.dgv_wobble_tag, true);
-                    } else if (mMobileItemId == INVALID_ID && child.getRotation() != 0) {
-                        child.setRotation(0);
-                        child.setTag(R.id.dgv_wobble_tag, false);
-                    }
-                }
-
             }
         }
 
@@ -1117,6 +1085,11 @@ public class DynamicGridView extends GridView {
         }
     };
 
+    /***
+     * 管理图标位置
+     * @author zhenghonglin_91
+     *
+     */
     private static class DynamicGridModification {
 
         private List<Pair<Integer, Integer>> transitions;
